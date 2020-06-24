06625ae8d16a4e352ee942602bed6a02d1c89ef7
yettas <ayazayazilim@gmail.com>
2020-06-24 18:52:36 +0300

    Gecmekalma.cpp

---
+++ gecmekalma.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+    cout << "Lutfen Notunuzu Giriniz" << endl;
+
+
+    int a;
+
+    cin >> a;
+
+
+    if (a > 45)
+    {
+        cout << "" << endl;
+        cout << "Geciyorsunuz" << endl;
+    }
+
+ else
+ {
+     cout << "" << endl;
+     cout << "Kaliyorsunuz" << endl;
+
+ }
+
+
+
+
+
+
+
+    return 0;
+}
